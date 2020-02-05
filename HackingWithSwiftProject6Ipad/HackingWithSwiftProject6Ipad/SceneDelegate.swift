a9814d419fbe946d5c8407039bacebae5eb4590a
Henry-chime chibuike <henry-chimechibuike@Henry-chimes-MacBook-Pro.local>
2020-02-05 16:21:48 +0100

    Ipad Game

--- HackingWithSwiftProject6Ipad/HackingWithSwiftProject6Ipad/SceneDelegate.swift
+++ HackingWithSwiftProject6Ipad/HackingWithSwiftProject6Ipad/SceneDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  SceneDelegate.swift
-//  HackingWithSwiftProject5(White House Petition)
+//  HackingWithSwiftProject6Ipad
 //
-//  Created by Henry-chime chibuike on 2/4/20.
+//  Created by Henry-chime chibuike on 2/5/20.
 //  Copyright © 2020 Henry-chime chibuike. All rights reserved.
 //
 
