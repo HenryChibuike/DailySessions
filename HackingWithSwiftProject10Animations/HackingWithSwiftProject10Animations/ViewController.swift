c0d7a5026e344017de5932114b4f58742425670d
Henry-chime chibuike <henry-chimechibuike@Henry-chimes-MacBook-Pro.local>
2020-02-11 09:56:01 +0100

    Update to save to Gallery

---
+++ HackingWithSwiftProject10Animations/HackingWithSwiftProject10Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackingWithSwiftProject10Animations
+//
+//  Created by Henry-chime chibuike on 2/11/20.
+//  Copyright © 2020 Henry-chime chibuike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HackingWithSwiftProject9Filter/HackingWithSwiftProject9Filter/UIVIew.swift
@@ -0,0 +1,9 @@
+//
+//  UIVIew.swift
+//  HackingWithSwiftProject9Filter
+//
+//  Created by Henry-chime chibuike on 2/11/20.
+//  Copyright © 2020 Henry-chime chibuike. All rights reserved.
+//
+
+import Foundation
